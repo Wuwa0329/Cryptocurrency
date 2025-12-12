91dfde256a5629f35e49f1c76b059af9c9ab034d
Ng Seng Kiat <wuwatest6@gmail.com>
2025-12-13 00:50:37 +0800

    convert json to sql then insert to Price table

--- Database.sql
+++ Database.sql
@@ -8,8 +8,6 @@ CREATE TABLE Price (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     Date datetime NOT NULL,
     Asset VARCHAR(50) NOT NULL,
+    Volume DECIMAL(20, 10) NOT NULL,
     Price DECIMAL(20, 10) NOT NULL
 );
-
--- Insert the static commodity data once
-INSERT INTO Metals (name, symbol, unit) VALUES ('Gold', 'XAU', 'Ounce');
--- poll_data.sh
+++ poll_data.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# BTC-USD
+# ./poll_data.sh BTC-USD 192.168.95.128 mysql_password
 instrument=$1
+mysql_host=$2
+mysql_password=$3
 
-curl "https://data-api.cryptocompare.com/index/cc/v1/historical/minutes?market=cadli&instrument=$instrument"
+
+curl -s "https://data-api.cryptocompare.com/index/cc/v1/historical/minutes?market=cadli&instrument=$instrument&limit=60&aggregate=1&fill=true&apply_mapping=true&response_format=JSON" | \
+    jq -r '.Data[] | 
+    "INSERT INTO Price (Date, Asset, Volume, Price) VALUES (FROM_UNIXTIME(\(.TIMESTAMP)), \"\(.INSTRUMENT)\", \(.VOLUME), \(.OPEN));"' | \
+    mysql -h $mysql_host -u ngsk -p Cryptocurrency --password=$mysql_password
